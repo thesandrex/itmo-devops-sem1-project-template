d24f10328cebc91c269f328b266be5a4601deb59
thesandrex <aleksanya2012@yandex.ru>
2025-01-18 18:49:42 +0400

    run fix

--- scripts/run.sh
+++ scripts/run.sh
@@ -12,7 +12,6 @@ if [ "$DEPLOYMENT_TYPE" == "local" ]; then
   cd ${REMOTE_DIR}
 
   export PGPASSWORD=$POSTGRES_PASSWORD
-  psql -h "$POSTGRES_HOST" -p "$POSTGRES_PORT" -U "$POSTGRES_USER" -d postgres -c "CREATE DATABASE $POSTGRES_DB;" || echo "Database $POSTGRES_DB already exists"
 
   go mod tidy
   go build -o app main.go
@@ -22,26 +21,6 @@ if [ "$DEPLOYMENT_TYPE" == "local" ]; then
 
   sleep 5
   curl -s http://localhost:8080/api/v0/prices || echo "Application is unavailable"
-
-elif [ "$DEPLOYMENT_TYPE" == "remote" ]; then
-
-  echo "Connecting to the remote server to set up and run the application..."
-  ssh ${REMOTE_USER}@${REMOTE_HOST} << EOF
-    set -e
-    cd ${REMOTE_DIR}
-
-    export PGPASSWORD=$POSTGRES_PASSWORD
-    psql -h "$POSTGRES_HOST" -p "$POSTGRES_PORT" -U "$POSTGRES_USER" -d postgres -c "CREATE DATABASE $POSTGRES_DB;" || echo "Database $POSTGRES_DB already exists"
-
-    go mod tidy
-    go build -o app main.go
-
-    nohup ./app > app.log 2>&1 &
-    echo "Application started"
-
-    sleep 5
-    curl -s http://localhost:8080/api/v0/prices || echo "Application is unavailable"
-  EOF
 
   echo "Application deployed successfully on ${REMOTE_HOST}."
   echo ${REMOTE_HOST}
