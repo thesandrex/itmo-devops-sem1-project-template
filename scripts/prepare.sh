8b6bc137039d74429274b8d2f17324fbde21fa58
thesandrex <aleksanya2012@yandex.ru>
2025-01-18 18:32:41 +0400

    prepare fix

--- scripts/prepare.sh
+++ scripts/prepare.sh
@@ -12,7 +12,7 @@ if [ "$DEPLOYMENT_TYPE" == "local" ]; then
   sudo apt-get install -y postgresql-contrib
 
   echo "Configuring PostgreSQL database and user..."
-  pg_isready -h localhost -p 5432 -U ${POSTGRES_USER}
+  pg_isready -h localhost -p 5432 -U "$POSTGRES_USER"
   sudo psql -h localhost -p 5432 -U ${POSTGRES_USER} --set=PGPASSWORD="${POSTGRES_PASSWORD}" -c "CREATE DATABASE ${POSTGRES_DB} OWNER ${POSTGRES_USER};"
   sudo psql -h localhost -p 5432 -U ${POSTGRES_USER} --set=PGPASSWORD="${POSTGRES_PASSWORD}" -c "GRANT ALL PRIVILEGES ON DATABASE ${POSTGRES_DB} TO ${POSTGRES_USER};"
 fi
