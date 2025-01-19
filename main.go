cae367d11a314a3cf819e2039f655db7bd389833
thesandrex <aleksanya2012@yandex.ru>
2025-01-19 14:56:19 +0400

    main.go update

--- main.go
+++ main.go
@@ -214,6 +214,10 @@ func processLinesAndInsert(db *sql.DB, lines []string) (int, int, float64, error
     totalCategories := make(map[string]struct{})
     totalPrice := 0.0
 
+    if lines[1] == "id,name,category,price,create_date" {
+        lines = lines[1:]
+    }
+
     for i, line := range lines {
         if line == "" {
             continue
